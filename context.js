b82c7ff7e4f35ac6fb4ea0c912fedd86d19861ae
Tramy Nguyen <tramynn2@gmail.com>
2019-08-16 15:19:00 -0500

    completed js 4 afternoon project

--- context.js
+++ context.js
@@ -57,7 +57,8 @@ let animal1 = new Animal('Tramy', 'homo sapien', 'coffee');
 ////////// PROBLEM 3 //////////
 
 /*
-  Use the bind method to assign context of the "this" keyword in the sayHi function to the user object; and save the bound function to a variable named whoSaysHi.  
+  Use the bind method to assign context of the "this" keyword in the sayHi function to the user object; 
+  and save the bound function to a variable named whoSaysHi.  
 */
 
 function sayHi(greeting) {
@@ -71,13 +72,14 @@ let who = {
 }
 
 //Code Here
-
+let whoSaysHi = sayHi.bind(who);
 
 
 ////////// PROBLEM 4 //////////
 
 /*
-  here we have a function that just returns the "this" keyword.  We will give context to "this", and your job is to tell us what the context is.
+  here we have a function that just returns the "this" keyword. 
+  We will give context to "this", and your job is to tell us what the context is.
 */
 
 function whatIsThis() {
@@ -85,7 +87,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = window
 
 let product = {
   name: 'snake plant',
@@ -96,7 +98,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -107,7 +109,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 
 class Family {
@@ -125,4 +127,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1
