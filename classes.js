21ea49720d25ee5d93c2b75095ec6d30df9c6b2c
Tramy Nguyen <tramynn2@gmail.com>
2019-08-16 15:47:40 -0500

    completed js 4 black diamond

--- classes.js
+++ classes.js
@@ -167,5 +167,29 @@ class ProgressiveManager extends Manager {
 */
 
 //Code Here
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
 
+  makeWidgets(num) {
+    this.widgets_made_count += num;
+    if (this.widgets_made_count % 50 === 0) {
+      this.wear_and_tear_count = this.widgets_made_count / 50;
+    }
+  }
+
+  fixMachine() {
+    this.needs_reboot = true;
+  }
+
+  reboot() {
+    return () => {
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    }
+  }
+}
 
