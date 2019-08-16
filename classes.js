4088ea21ae5b1280e62a3995ee290bac3f3ba628
Tramy Nguyen <tramynn2@gmail.com>
2019-08-16 14:08:09 -0500

    completed classes problem 1 and 2

--- classes.js
+++ classes.js
@@ -30,6 +30,18 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget() {
+    return this.first_name + ' ' + this.last_name + ' ' + 'Widget';
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -48,7 +60,19 @@
 */
 
 //Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
 
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
